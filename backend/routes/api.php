c52c108a0a74e6d8041afa7b87a124f6f76ce637
Ahmed <ahmedchouihi@gmail.com>
2021-06-19 16:13:52 +0100

    'addBackendEntraineur'

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,8 +14,14 @@ Route::group([
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::get('user-profile', 'AuthController@userProfile');
+    //adherent
     Route::get('adherent', 'AdherentController@index')->middleware('auth:api');
     Route::put('updateadherent/{id}', 'AdherentController@updateadherent')->middleware('auth:api');
     Route::delete('delete/{id}', 'AdherentController@destroy')->middleware('auth:api');
     Route::post('addadherent', 'AdherentController@store')->middleware('auth:api');
+    //entraineur
+    Route::get('entraineur', 'EntraineurController@index')->middleware('auth:api');
+    Route::put('updateentraineur/{id}', 'EntraineurController@updateentraineur')->middleware('auth:api');
+    Route::delete('deleteentraineur/{id}', 'EntraineurController@destroy')->middleware('auth:api');
+    Route::post('addentraineur', 'EntraineurController@store')->middleware('auth:api');
 });
